77983d44569a5cea720ff07b21585b71dea7a999
Yeasir Arefin Tusher <yeasir.arefin@bjitacademy.com>
2023-02-09 17:02:19 +0600

    first commit

---
+++ CricPulse/Controllers/MainViewController/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CricPulse
+//
+//  Created by bjit on 9/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
