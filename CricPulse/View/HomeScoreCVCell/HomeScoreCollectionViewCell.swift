8407e2fe3e8452c9d0dc48a9c02309841881bc11
Yeasir Arefin Tusher <tusher15-5944@diu.edu.bd>
2023-02-12 12:45:38 +0600

    Ui Changes

---
+++ CricPulse/View/HomeScoreCVCell/HomeScoreCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeScoreCollectionViewCell.swift
+//  CricPulse
+//
+//  Created by Yeasir Arefin Tusher on 11/2/23.
+//
+
+import UIKit
+
+class HomeScoreCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- CricPulse/View/ScoreCard/ScoreCardCollectionViewCell.swift
+++ CricPulse/View/ScoreCard/ScoreCardCollectionViewCell.swift
@@ -16,14 +16,17 @@ class ScoreCardCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var tournamentTitle: UILabel!
     @IBOutlet weak var matchNo: UILabel!
     @IBOutlet weak var matchStatusLabel: UILabel!  // Live or Upcoming or Completed
+    @IBOutlet weak var stadiumName: UILabel!
     
-    
+    // Button Outlets
+    @IBOutlet weak var notificationButtonOutlet: UIButton!
     
     
     override func awakeFromNib() {
         super.awakeFromNib()
         backView.round(10)
         backView.addBorder(color: UIColor.systemGray6, width: 1)
+        notificationButtonOutlet.round(5)
         
         MatchStatusView.round(5)
     }
