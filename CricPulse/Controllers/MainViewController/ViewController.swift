3c050cd7f4ee20a74a5e3bf1b7dcd4357fe6007c
Yeasir Arefin Tusher <yeasir.arefin@bjitacademy.com>
2023-02-13 11:56:45 +0600

    CollectionView Working

--- CricPulse/Controllers/MainViewController/ViewController.swift
+++ CricPulse/Controllers/MainViewController/ViewController.swift
@@ -1,10 +1,17 @@
 
 import UIKit
 class ViewController: UIViewController {
-
+    // ViewModel
+    let mainViewModel = MainViewModel()
+    // Outlets
+    @IBOutlet weak var HomeCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ConfigureViewDidLoad()
     }
-
+    
+    fileprivate func ConfigureViewDidLoad(){
+        setupCollectionView()
+    }
 }
-
