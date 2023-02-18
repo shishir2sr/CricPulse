41e8760edcdc0d2d95c1c3a149852086c3192df8
Yeasir Arefin Tusher <tusher15-5944@diu.edu.bd>
2023-02-18 17:15:54 +0600

    UI Changes

--- CricPulse/Controllers/MatchDetailsVC/MatchDetailsViewController.swift
+++ CricPulse/Controllers/MatchDetailsVC/MatchDetailsViewController.swift
@@ -1,10 +1,14 @@
 import UIKit
 
 class MatchDetailsViewController: UIViewController {
+    // MARK: - Outlets
     // View Outlets
     @IBOutlet weak var navigationView: UIView!
     @IBOutlet weak var backView: UIView!
     @IBOutlet weak var matchStatusView: UIView!
+    @IBOutlet weak var noteView: UIView!
+    @IBOutlet weak var scoreView: UIView!
+    
     // Label outlets
     @IBOutlet weak var tournamentName: UILabel!
     @IBOutlet weak var matchNumber: UILabel!
@@ -14,18 +18,32 @@ class MatchDetailsViewController: UIViewController {
     @IBOutlet weak var teamOneFlag: UIImageView!
     @IBOutlet weak var teamOneCode: UILabel!
     @IBOutlet weak var teamOneScore: UILabel!
+    @IBOutlet weak var teamOneWInPercentage: UILabel!
+    
     @IBOutlet weak var teamTwoFlag: UIImageView!
     @IBOutlet weak var teamTwoCode: UILabel!
     @IBOutlet weak var teamTwoScore: UILabel!
+    @IBOutlet weak var teamTwoWinPercentage: UILabel!
     
     @IBOutlet weak var matchType: UILabel!
-    
+    @IBOutlet weak var matchDate: UILabel!
+    @IBOutlet weak var note: UILabel!
+    @IBOutlet weak var winProgress: UIProgressView!
     
     // Stack Outlets
+    @IBOutlet weak var scoreStack: UIStackView!
+    @IBOutlet weak var winPercenTageStackView: UIStackView!
+    // Segment Controller
+    @IBOutlet weak var segmentController: UISegmentedControl!
+    
+    // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         backView.round(10)
+        matchType.addBorder(color: .systemGreen, width: 1)
+        matchType.round(5)
+        matchStatusView.round(5)
+        noteView.round(5)
+        scoreView.round(5)
     }
-
 }
