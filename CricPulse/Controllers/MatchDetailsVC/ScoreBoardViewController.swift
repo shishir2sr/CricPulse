1ddac7883ed0b168c820a35169c1c759910e32cf
Yeasir Arefin Tusher <tusher15-5944@diu.edu.bd>
2023-02-19 11:47:48 +0600

    UI: Details page UI

--- CricPulse/Controllers/MatchDetailsVC/ScoreBoardViewController.swift
+++ CricPulse/Controllers/MatchDetailsVC/ScoreBoardViewController.swift
@@ -8,11 +8,39 @@ class ScoreBoardViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        teamOneBackgroundVIew.addBorder(color: .systemGray5, width: 0.5)
+        teamOneBackgroundVIew.addBorder(color: .systemGray4, width: 1)
         teamOneBackgroundVIew.round(10)
-        teamTwoBackgroundVIew.addBorder(color: .systemGray5, width: 1)
+        teamOneBackgroundVIew.addShadow(opacity: 1, offset: 6, radius: 4)
+        
+        teamTwoBackgroundVIew.addBorder(color: .systemGray4, width: 1)
         teamTwoBackgroundVIew.round(10)
+        
+        let tapGestureTwo = UITapGestureRecognizer(target: self, action: #selector(viewTwoTapped))
+        teamTwoBackgroundVIew.addGestureRecognizer(tapGestureTwo)
+        
+        let tapGestureOne = UITapGestureRecognizer(target: self, action: #selector(viewOneTapped))
+        teamOneBackgroundVIew.addGestureRecognizer(tapGestureOne)
+        
     }
     
-
+    // View One tapped
+    @objc func viewOneTapped(){
+        buttonGuestureAnimation(for: teamOneBackgroundVIew)
+    }
+    
+    // Team two button tap
+    @objc func viewTwoTapped(){
+        print("button 2 pressed")
+        buttonGuestureAnimation(for: teamTwoBackgroundVIew)
+    }
+//     Animation
+    fileprivate func buttonGuestureAnimation(for button: UIView) {
+        UIView.animate(withDuration: 0.1, animations: {
+            button.backgroundColor = UIColor.systemGray5
+        }, completion: { _ in
+            UIView.animate(withDuration: 0.1, animations: {
+                button.backgroundColor = UIColor.white
+            })
+        })
+    }
 }
--- CricPulse/Shared/AppExtensions/UIViewExtension.swift
+++ CricPulse/Shared/AppExtensions/UIViewExtension.swift
@@ -2,15 +2,26 @@ import Foundation
 import UIKit
 
 extension UIView {
+    /// Cornerradius
     func round(_ radious: CGFloat = 10) {
         layer.cornerRadius = radious
         clipsToBounds = true
     }
-
+    
+    /// Add border to uiviews
     func addBorder(color: UIColor, width: CGFloat) {
         layer.borderColor = color.cgColor
         layer.borderWidth = width
     }
+    /// Add shadows to uiviews
+    func addShadow(opacity: Float, offset: Double, radius: Double) {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = opacity
+        layer.shadowOffset = CGSize(width: 0, height: offset)
+        layer.shadowRadius = CGFloat(radius)
+    }
+    
+    
 }
 
 
