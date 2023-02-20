395fb50de3e688ae59e47a9dcd046aec5cb187c7
Yeasir Arefin Tusher <yeasir.arefin@bjitacademy.com>
2023-02-20 11:25:02 +0600

    Footer: added

--- CricPulse/View/MatchDetailsView/FooterForBattersCell.swift
+++ CricPulse/View/MatchDetailsView/FooterForBattersCell.swift
@@ -2,5 +2,11 @@
 import UIKit
 
 class FooterForBattersCell: UITableViewHeaderFooterView {
-
+    @IBOutlet weak var totalExtraRuns: UILabel!
+    @IBOutlet weak var extraRuns: UILabel! // (b: 1, lb: 1, nb: 1, wd: 16)
+    @IBOutlet weak var totalRun: UILabel! // Format (Total: 138/9)
+    @IBOutlet weak var oversPlayed: UILabel! // Overs: 20.0
+    @IBOutlet weak var runRate: UILabel! // (RR: 8.1)
+    
+    
 }
