e7d841849b3cca17ac4ec7712e4c071e39f20c29
Yeasir Arefin Tusher <tusher15-5944@diu.edu.bd>
2023-02-21 10:25:31 +0600

    DATACLASS: Batsman DataClass

---
+++ CricPulse/Model/Players Repository/BatsManDataClass.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+struct BatsManDataClass: Decodable {
+    let id: Int?
+    let sort: Int?
+    let fixture_id: Int?
+    let team_id: Int?
+    let player_id: Int?
+    let ball: Int?
+    let score_id: Int?
+    let score: Double?
+    let four_x: Int?
+    let six_x: Int?
+    let rate: Double?
+    let updated_at: Date?
+    let batsman: Player?
+}
