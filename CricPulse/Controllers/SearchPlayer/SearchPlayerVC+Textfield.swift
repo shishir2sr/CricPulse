f3926518aafb55672b392c6c0def106b7326e52c
Yeasir Arefin Tusher <yeasir.arefin@bjitacademy.com>
2023-02-23 15:58:59 +0600

    Return Working

--- CricPulse/Controllers/SearchPlayer/SearchPlayerVC+Textfield.swift
+++ CricPulse/Controllers/SearchPlayer/SearchPlayerVC+Textfield.swift
@@ -7,26 +7,41 @@ extension SearchPlayerViewController: UITextFieldDelegate{
         searchBar.delegate = self
     }
     
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let searchText = textField.text else {return true}
+        self.searchPlayerViewModel.playersData = CoreDataHelper.shared.searchPlayerbyName(with: searchText)
+        textField.endEditing(true)
+        textField.resignFirstResponder()
+        return true
+    }
     
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         if textField.text != ""{
             return true
         }
         else{
-            textField.placeholder = "Write a player name"
+            textField.placeholder = "Write a players name"
             return false
         }
     }
     
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.text != ""{
+            guard let searchText = textField.text else {return}
+            self.searchPlayerViewModel.playersData = CoreDataHelper.shared.searchPlayerbyName(with: searchText)
+        }
+       
+        Task{await self.searchPlayerViewModel.getPlayers()}
+        textField.text = ""
+    }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let text = (textField.text! as NSString).replacingCharacters(in: range, with: string)
         if text != ""{
             self.searchPlayerViewModel.playersData = CoreDataHelper.shared.searchPlayerbyName(with: text)
-            self.reloadTableView()
         }
         return true
     }
+    
     
 }
