fa5ff0ac6f578ad07210265897c2d63e701b697f
Yeasir Arefin Tusher <tusher15-5944@diu.edu.bd>
2023-02-25 12:35:23 +0600

    UI: CHange

--- CricPulse/Controllers/FixtureViewController/FixtureVC+TV.swift
+++ CricPulse/Controllers/FixtureViewController/FixtureVC+TV.swift
@@ -16,10 +16,14 @@ extension FixtureViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell  = tableView.dequeueReusableCell(withIdentifier: FinishedMatchScoreCardTVModel.identifier, for: indexPath) as? HomeTableViewCell
         guard let cell = cell else{return UITableViewCell()}
+        
+        cell.addShadow(opecity: 0.8, size: 1, radius: 2, color: UIColor.gray)
+        cell.selectionStyle = .none
         return cell
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        150
+       145
     }
     
     
