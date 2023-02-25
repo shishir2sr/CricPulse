89c8eb8398a6312671dc35d45a4dd76d91b50106
Yeasir Arefin Tusher <tusher15-5944@diu.edu.bd>
2023-02-25 11:13:05 +0600

    UI: Fixture

---
+++ CricPulse/Controllers/FixtureViewController/FixtureViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+class FixtureViewController: UIViewController {
+    
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
