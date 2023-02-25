64d25349544d6ee4c33f1f49a3a03de30d7df562
Yeasir Arefin Tusher <tusher15-5944@diu.edu.bd>
2023-02-25 11:47:36 +0600

    TableView Added

--- CricPulse/Controllers/FixtureViewController/FixtureViewController.swift
+++ CricPulse/Controllers/FixtureViewController/FixtureViewController.swift
@@ -2,6 +2,9 @@ import UIKit
 class FixtureViewController: UIViewController {
     
     @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var fromDate: UIDatePicker!
+    @IBOutlet weak var toDate: UIDatePicker!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
